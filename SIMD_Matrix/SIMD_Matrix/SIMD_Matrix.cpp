bfca54b48acd7238413cbb056dedb1328c83c790
horiK <hazemi94basic@gmail.com>
2023-10-06 00:57:05 +0900

    main()に書いていた計算を削除

--- SIMD_Matrix/SIMD_Matrix/SIMD_Matrix.cpp
+++ SIMD_Matrix/SIMD_Matrix/SIMD_Matrix.cpp
@@ -11,34 +11,4 @@ int main()
 	//テストの実行
 	doctest::Context context;
 	context.run();
-
-	using namespace std;
-
-	Float4 a(1, 2, 3, 4);
-	Float4 b(-2.5f, 5, -7.5f, 10);
-
-	cout << Float4(Float3(1, 2, 3), -1) << endl;
-
-	cout << (a + b) << endl;
-	cout << (a - b) << endl;
-	cout << +a << endl;
-	cout << -a << endl;
-	cout << (a * 0.5) << endl;
-	cout << (0.5 * a) << endl;
-	cout << (a / 2) << endl;
-	cout << (Float4::Dot(a, b)) << endl;
-	cout << endl;
-	a += b;
-	cout << a << endl;
-	a -= b;
-	cout << a << endl;
-	a *= 2;
-	cout << a << endl;
-	a /= 2;
-	cout << a << endl;
-
-	Float4 f = (a += b) += b;
-	cout << f << endl;
-	cout << endl;
-	cout << endl;
 }
