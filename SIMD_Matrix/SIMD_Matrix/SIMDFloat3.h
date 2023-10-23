4e7fefbe2f0f64ba66b4250bd7eecc049da05730
horiK <hazemi94basic@gmail.com>
2023-10-24 02:27:52 +0900

    SIMD命令を用いたFloat3構造体の関数実装

--- SIMD_Matrix/SIMD_Matrix/SIMDFloat3.h
+++ SIMD_Matrix/SIMD_Matrix/SIMDFloat3.h
@@ -205,14 +205,15 @@ inline bool SIMDFloat3::equal(const SIMDFloat3& v, const float epsilon) const
 {
 	__m128 a = _mm_load_ps(e);
 	__m128 b = _mm_load_ps(v.e);
-	__m128 abs = _mm_add_ps(a, b);
+	__m128 sub = _mm_sub_ps(a, b);
+	__m128 abs = _mm_add_ps(sub, sub);
 
-	__m128 epsi = _mm_set1_ps(epsilon);
+	__m128 epsi = _mm_set1_ps(epsilon * epsilon);
 	__m128 isUnder = _mm_cmple_ps(abs, epsi);
 
 	float num[4];
 	_mm_store_ps(num, isUnder);
-	return num[0] + num[1] + num[2] == 0;
+	return num[0] && num[1] && num[2];
 }
 
 inline bool SIMDFloat3::isZero() const
@@ -223,37 +224,88 @@ inline bool SIMDFloat3::isZero() const
 
 	float num[4];
 	_mm_store_ps(num, isEqual);
-	return num[0] + num[1] + num[2] == 0;
+	return num[0] && num[1] && num[2];
 }
 
 inline float SIMDFloat3::length() const
 {
-	return 0.0f;
+	return sqrt(squareLength());
 }
 
 inline float SIMDFloat3::squareLength() const
 {
-	return 0.0f;
+	__m128 a = _mm_setr_ps(e[0], e[1], e[2], 0);
+	__m128 mul = _mm_mul_ps(a, a);
+
+	__m128 movehlAdd = _mm_add_ps(mul, _mm_movehl_ps(mul, mul));
+
+	__m128 revMoveHl = _mm_shuffle_ps(movehlAdd, movehlAdd, 0b1);
+	__m128 sqrtLength = _mm_add_ss(movehlAdd, revMoveHl);
+
+	float num[4];
+	_mm_store_ps(num, sqrtLength);
+	return num[0];
 }
 
 inline SIMDFloat3& SIMDFloat3::normalize()
 {
+	__m128 a = _mm_load_ps(e);
+	__m128 k = _mm_set1_ps(1 / length());
+
+	__m128 mul = _mm_mul_ps(a, k);
+
+	float num[4];
+	_mm_store_ps(num, mul);
+
+	*this = SIMDFloat3(num);
 	return *this;
 }
 
 inline float SIMDFloat3::Dot(const SIMDFloat3& v1, const SIMDFloat3& v2)
 {
-	return 0.0f;
+	__m128 a = _mm_setr_ps(v1.e[0], v1.e[1], v1.e[2], 0);
+	__m128 b = _mm_setr_ps(v2.e[0], v2.e[1], v2.e[2], 0);
+	__m128 mul = _mm_mul_ps(a, b);
+
+	__m128 movehlAdd = _mm_add_ps(mul, _mm_movehl_ps(mul, mul));
+
+	__m128 revMoveHl = _mm_shuffle_ps(movehlAdd, movehlAdd, 0b1);
+	__m128 dot = _mm_add_ss(movehlAdd, revMoveHl);
+
+	float num[4];
+	_mm_store_ps(num, dot);
+	return num[0];
 }
 
 inline SIMDFloat3 SIMDFloat3::Cross(const SIMDFloat3& v1, const SIMDFloat3& v2)
 {
-	return SIMDFloat3();
+	__m128 a2a3a1 = _mm_set_ps(0, v1.e[0], v1.e[2], v1.e[1]);
+	__m128 b3b1b2 = _mm_set_ps(0, v2.e[1], v2.e[0], v2.e[2]);
+	__m128 a3a1a2 = _mm_set_ps(0, v1.e[1], v1.e[0], v1.e[2]);
+	__m128 b2b3b1 = _mm_set_ps(0, v2.e[0], v2.e[2], v2.e[1]);
+
+	__m128 leftMulti = _mm_mul_ps(a2a3a1, b3b1b2);
+	__m128 rightMulti = _mm_mul_ps(a3a1a2, b2b3b1);
+
+	__m128 cross = _mm_sub_ps(leftMulti, rightMulti);
+
+	float num[4];
+	_mm_store_ps(num, cross);
+
+	return SIMDFloat3(num);
 }
 
 inline SIMDFloat3 SIMDFloat3::Normalize(const SIMDFloat3& v)
 {
-	return SIMDFloat3();
+	__m128 a = _mm_load_ps(v.e);
+	__m128 k = _mm_set1_ps(1 / v.length());
+
+	__m128 mul = _mm_mul_ps(a, k);
+
+	float num[4];
+	_mm_store_ps(num, mul);
+
+	return SIMDFloat3(num);
 }
 
 inline std::ostream& operator<<(std::ostream& stream, const SIMDFloat3& v)
